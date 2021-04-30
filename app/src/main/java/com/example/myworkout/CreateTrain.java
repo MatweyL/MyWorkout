3c9e6ea635da87d9460bd2015e08dfb04778d4e0
User <matweyka2003@mail.ru>
2021-04-30 22:59:47 +0300

    Дальше будут изменения

---
+++ app/src/main/java/com/example/myworkout/CreateTrain.java
@@ -0,0 +1,32 @@
+package com.example.myworkout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class CreateTrain extends Fragment {
+
+    private FloatingActionButton buttonAddExercise;
+    private RecyclerView recyclerView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_train, container, false);
+    }
+}
