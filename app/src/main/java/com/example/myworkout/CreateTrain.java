2c612ae1cc274638ea60768480fe3cf8cebff444
User <matweyka2003@mail.ru>
2021-05-01 12:18:52 +0300

    На лэйаутах

--- app/src/main/java/com/example/myworkout/CreateTrain.java
+++ app/src/main/java/com/example/myworkout/CreateTrain.java
@@ -1,32 +1,30 @@
 package com.example.myworkout;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-
-public class CreateTrain extends Fragment {
-
+public class CreateTrain extends AppCompatActivity {
     private FloatingActionButton buttonAddExercise;
     private RecyclerView recyclerView;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_train);
+        recyclerView = findViewById(R.id.recycler_exercises);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        buttonAddExercise = findViewById(R.id.add_exercise);
+        buttonAddExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_train, container, false);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myworkout/MainActivity.java
+++ app/src/main/java/com/example/myworkout/MainActivity.java
@@ -3,29 +3,29 @@ package com.example.myworkout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    //private MainActivityMenu mainActivityMenuFragment;
+    private Button bTrain, bCreateTrain, bSeeTrains, bSeeExercises, bSome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try{
-            MainActivityMenu mainActivityMenuFragment = new MainActivityMenu();
-            FragmentTransaction fr = getSupportFragmentManager().beginTransaction();
-            fr.replace(R.id.container_main, mainActivityMenuFragment);
-            fr.commit();
-        }catch (Exception e){
-            Log.i("In transaction", "blin(");
-        }
-
+        bTrain = findViewById(R.id.button_train);
+        bCreateTrain = findViewById(R.id.button_create_train);
+        bSeeTrains = findViewById(R.id.button_trains_list);
+        bSeeExercises = findViewById(R.id.button_show_exercises_list);
+        bSome = findViewById(R.id.button_some_b);
+        bCreateTrain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateTrain.class);
+                startActivity(intent);
+            }
+        });
     }
 }
-/*
-com.example.myworkout public class MainActivityMenu
-extends Fragment
-A simple Fragment subclass. Use the MainActivityMenu.newInstance factory method to create an instance of this fragment.
-  MyWorkout.app
- */
