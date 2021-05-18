0c0f11189fa248bdc3e36dbfa1333b81ca775637
Ivan <iv.cherepanov@yandex.ru>
2021-05-18 12:18:33 +0300

    fix exercise

--- app/src/main/java/com/example/myworkout/ReadActivity.java
+++ app/src/main/java/com/example/myworkout/ReadActivity.java
@@ -2,6 +2,7 @@ package com.example.myworkout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -90,15 +91,21 @@ public class ReadActivity extends AppCompatActivity {
     }
 
     private void search1(String newText) {
-        for(String object: title_list){
-            //System.out.println(object);
-            if(object.contains(newText.toLowerCase())){
+        if(title_list.contains(newText)){
+            Toast.makeText(ReadActivity.this,"Есть!",Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(ReadActivity.this,"Мы еще не добавили!",Toast.LENGTH_SHORT).show();
+        }
+        /*for(String object: title_list){
+            Log.d("STRING:", object);
+            if(newText.toLowerCase().contains(object.toLowerCase())){
                 Toast.makeText(ReadActivity.this,"Есть!",Toast.LENGTH_LONG).show();
             }
             else{
                 Toast.makeText(ReadActivity.this,"Мы еще не добавили!",Toast.LENGTH_SHORT).show();
             }
-        }
+        }*/
 
     }
 
