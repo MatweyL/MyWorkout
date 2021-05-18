53446cdd3e5dbb6407bf5719cd129ebb33486c0a
User <matweyka2003@mail.ru>
2021-05-18 12:57:02 +0300

    Добавлен нужный Create_Exercise_Activity

--- app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/Create_Exercise_Activity.java
+++ app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/Create_Exercise_Activity.java
@@ -18,7 +18,7 @@ import com.example.myworkout.R;
 
 public class Create_Exercise_Activity extends AppCompatActivity {
     EditText etName, etNumberReps, etNumberSets, etTimeExercise, etTimeRest;
-    Button btn_save;
+    Button btn_save, bp1, bp2, bp3, bp4, bm1, bm2, bm3, bm4;
     ExerciseDao exerciseDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +27,13 @@ public class Create_Exercise_Activity extends AppCompatActivity {
         exerciseDao = DBHelper.getInstance(getApplicationContext()).getAppDatabase().getExerciseDao();
         Train train = (Train) getIntent().getExtras().getSerializable(Train.class.getSimpleName());
         long trainID =train.getId();
-        etName =findViewById(R.id.editTextName);
-        etNumberReps = findViewById(R.id.editTextNumberOfReps);
-        etNumberSets = findViewById(R.id.editTextNumberOfSets);
-        etTimeExercise = findViewById(R.id.editTextTimeOfExercise);
-        etTimeRest = findViewById(R.id.editTextTimeOfRest);
-        btn_save = findViewById(R.id.button_save_exercise);
+        findAllWidgets();
+        setButtonsCommand();
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkData(etName, etNumberReps, etNumberSets, etTimeRest)){
-                    String timeExercise = getTimeExercise(etTimeExercise);
-                    Exercise exercise = new Exercise(0, trainID, getString(etName), getString(etNumberReps), getString(etNumberSets), timeExercise, getString(etTimeRest));
+                if (checkData(etName, etNumberReps, etNumberSets, etTimeRest, etTimeExercise)){
+                    Exercise exercise = new Exercise(0, trainID, getString(etName), getString(etNumberReps), getString(etNumberSets), getString(etTimeExercise), getString(etTimeRest));
                     addExerciseInThread(exercise);
                     increaseTimeTraining(train, exercise);
                     updateTrainTimeInThread(train);
@@ -53,20 +48,31 @@ public class Create_Exercise_Activity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    private void findAllWidgets(){
+        etName =findViewById(R.id.editTextName);
+        etNumberReps = findViewById(R.id.editTextNumberOfReps);
+        etNumberSets = findViewById(R.id.editTextNumberOfSets);
+        etTimeExercise = findViewById(R.id.editTextNumberOfWeight);
+        etTimeRest = findViewById(R.id.editTextTimeOfRest);
+        btn_save = findViewById(R.id.button_save_exercise);
+        bp1 = findViewById(R.id.button_p1c);
+        bm1 = findViewById(R.id.button_m1c);
+        bp2 = findViewById(R.id.button_p2c);
+        bm2 = findViewById(R.id.button_m2c);
+        bp3 = findViewById(R.id.button_p3c);
+        bm3 = findViewById(R.id.button_m3c);
+        bp4 = findViewById(R.id.button_p4c);
+        bm4 = findViewById(R.id.button_m4c);
     }
 
     private void increaseTimeTraining(Train train, Exercise exercise){
         train.setTimeOfTraining(Long.toString(Long.parseLong(train.getTimeOfTraining()) +
-                Long.parseLong(exercise.getSetsNumber())*(Long.parseLong(exercise.getTimeRest()) + getTimeExLong(exercise.getTimeExercise()))) );
+                Long.parseLong(exercise.getSetsNumber())*(Long.parseLong(exercise.getTimeRest()))) );
     }
-    private long getTimeExLong(String timeEx){
-        if (timeEx.equals("без времени")){
-            return 0;
-        }
-        else {
-            return Long.parseLong(timeEx);
-        }
-    }
+
 
     private void addExerciseInThread(Exercise exercise)
     {
@@ -80,19 +86,20 @@ public class Create_Exercise_Activity extends AppCompatActivity {
     private String getString(EditText et){
         return et.getText().toString();
     }
-
-    private String getTimeExercise(EditText et){
-        if (TextUtils.isEmpty(et.getText().toString())){
-            return  "без времени";
+    private String getString(EditText et, int defaultValue){
+        if (TextUtils.isEmpty(et.getText().toString())) {
+            return Integer.toString(defaultValue);
         }
         else {
-            return etTimeExercise.getText().toString();
+            return et.getText().toString();
         }
     }
 
-    private boolean checkData(EditText nm, EditText nR, EditText nS, EditText tR){
-        return !(TextUtils.isEmpty(nm.getText().toString()) || TextUtils.isEmpty(nR.getText().toString()) ||
-                TextUtils.isEmpty(nS.getText().toString())|| TextUtils.isEmpty(tR.getText().toString()));
+    private boolean checkData(EditText nm, EditText nR, EditText nS, EditText tR, EditText nW){
+        return !(TextUtils.isEmpty(getString(nm)) || TextUtils.isEmpty(getString(nR)) ||
+                TextUtils.isEmpty(getString(nS))|| TextUtils.isEmpty(getString(tR))
+                || TextUtils.isEmpty(getString(nW)) || getString(nR).equals("0")
+                || getString(nS).equals("0") || getString(tR).equals("0"));
     }
 
     private void updateTrainTimeInThread(Train train) {
@@ -102,5 +109,63 @@ public class Create_Exercise_Activity extends AppCompatActivity {
                 DBHelper.getInstance(getApplicationContext()).getAppDatabase().getTrainDao().updateTrain(train);
             }
         }).start();
+    }
+    private void setButtonsCommand(){
+        bm1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.parseInt(getString(etNumberReps)) > 1) {
+                    etNumberReps.setText(Integer.toString(-1 + Integer.parseInt(getString(etNumberReps, 0))));
+                }
+            }
+        });
+        bm2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.parseInt(getString(etNumberSets)) > 1) {
+                    etNumberSets.setText(Integer.toString(-1 + Integer.parseInt(getString(etNumberSets, 0))));
+                }
+            }
+        });
+        bm3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.parseInt(getString(etTimeExercise)) > 0) {
+                    etTimeExercise.setText(Integer.toString(-1 + Integer.parseInt(getString(etTimeExercise, 0))));
+                }
+            }
+        });
+        bm4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.parseInt(getString(etTimeRest)) > 1) {
+                    etTimeRest.setText(Integer.toString(-1 + Integer.parseInt(getString(etTimeRest, 0))));
+                }
+            }
+        });
+        bp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etNumberReps.setText(Integer.toString(1 + Integer.parseInt(getString(etNumberReps, 0))));
+            }
+        });
+        bp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etNumberSets.setText(Integer.toString(1 + Integer.parseInt(getString(etNumberSets, 0))));
+            }
+        });
+        bp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etTimeExercise.setText(Integer.toString(1 + Integer.parseInt(getString(etTimeExercise, 0))));
+            }
+        });
+        bp4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etTimeRest.setText(Integer.toString(1 + Integer.parseInt(getString(etTimeRest, 0))));
+            }
+        });
     }
 }
