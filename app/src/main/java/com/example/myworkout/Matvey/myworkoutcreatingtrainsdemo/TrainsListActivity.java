8cac4f96f491afdded5cecd5de86019f5e6bf0d9
User <matweyka2003@mail.ru>
2021-05-21 22:04:48 +0300

    Подработаны дизайны

--- app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/TrainsListActivity.java
+++ app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/TrainsListActivity.java
@@ -1,11 +1,16 @@
 package com.example.myworkout.Matvey.myworkoutcreatingtrainsdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myworkout.MainActivity;
 import com.example.myworkout.Matvey.myworkoutcreatingtrainsdemo.adapters.TrainAdapter;
 import com.example.myworkout.Matvey.myworkoutcreatingtrainsdemo.room.DBHelper;
 import com.example.myworkout.Matvey.myworkoutcreatingtrainsdemo.room.Train;
@@ -18,6 +23,7 @@ public class TrainsListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private List<Train> trains;
     private boolean training;
+    private ImageButton button_to_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,14 @@ public class TrainsListActivity extends AppCompatActivity {
         training = getIntent().getExtras().getBoolean("training", false);
         TrainAdapter adapter = new TrainAdapter(this, trains, training);
         recyclerView.setAdapter(adapter);
-
+        button_to_menu = findViewById(R.id.button_back_to_main_menuM);
+        button_to_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TrainsListActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void prepareData() {
