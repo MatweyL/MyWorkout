6f80bad6b098486fb4cf813cc0848db69c7212a0
User <matweyka2003@mail.ru>
2021-05-21 17:49:49 +0300

    Добавлена возможность поделиться тренировкой в соцсетях

--- app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/TrainExercisesActivity.java
+++ app/src/main/java/com/example/myworkout/Matvey/myworkoutcreatingtrainsdemo/TrainExercisesActivity.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class TrainExercisesActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private TextView trainNameView, targetMusclesView, textViewTimeOfTrainMinutes2, textViewTimeOfTrainSeconds2;
-    private Button btn_update;
+    private Button btn_update, btn_share;
     private List<Exercise> exercises;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +56,32 @@ public class TrainExercisesActivity extends AppCompatActivity {
                 intent.putExtra(Train.class.getSimpleName(), train);
                 intent.putExtra("ActivityName", "TrainExercisesActivity");
                 startActivity(intent);
+            }
+        });
+
+        btn_share = findViewById(R.id.button_share_train);
+        btn_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuffer message = new StringBuffer();
+                message.append("Составил крутую тренировку в приложении MyWorkout, оцените)\n");
+                message.append("По времени она занимает около ").append(trTimeMinutes).append(" мин. ").append(trTimeSeconds).append(" сек").append('\n');
+                message.append("Целевые мышцы: ");
+                message.append(train.getTargetMuscles()).append('\n').append("Вот список упражнений:").append('\n');
+                for (Exercise ex: exercises) {
+                    message.append(ex.getName()).append(": ").append(ex.getSetsNumber()).append('x').append(ex.getRepsNumber()).append(", ");
+                    if (!ex.getTimeExercise().equals("0")) {
+                        message.append("вес снаряда - ").append(ex.getTimeExercise()).append(" кг, ");
+                    }
+                    message.append("отдых между подходами - ").append(ex.getTimeRest()).append(" c;").append('\n');
+                }
+                message.append("Пойду тренироваться!");
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String msg =  message.toString();
+                intent.putExtra(Intent.EXTRA_TEXT, msg);
+                Intent selectIntent = Intent.createChooser(intent, "Как вы хотите поделиться тренировкой&");
+                startActivity(selectIntent);
             }
         });
     }
